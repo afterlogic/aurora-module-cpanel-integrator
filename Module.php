6ecbf236bf9a1610c58a9ef9a2dd0f5acc6f9dd5
artemij-nemec <artem@chesnokov.pp.ua>
2019-05-14 16:27:14 +0300

    Added forward and autoresponder functionality

--- Module.php
+++ Module.php
@@ -25,6 +25,10 @@ class Module extends \Aurora\System\Module\AbstractModule
 		$this->subscribeEvent('MailSignup::Signup::before', [$this, 'onAfterSignup']);
 		$this->subscribeEvent('Mail::Account::ToResponseArray', array($this, 'onMailAccountToResponseArray'));
 		$this->subscribeEvent('Mail::ChangeAccountPassword', array($this, 'onChangeAccountPassword'));
+		$this->subscribeEvent('Mail::UpdateForward::before', array($this, 'onBeforeUpdateForward'));
+		$this->subscribeEvent('Mail::GetForward::before', array($this, 'onBeforeGetForward'));
+		$this->subscribeEvent('Mail::GetAutoresponder::before', array($this, 'onBeforeGetAutoresponder'));
+		$this->subscribeEvent('Mail::UpdateAutoresponder::before', array($this, 'onBeforeUpdateAutoresponder'));
 	}
 
 	public function getCpanel()
@@ -190,6 +194,242 @@ class Module extends \Aurora\System\Module\AbstractModule
 		return $bBreakSubscriptions;
 	}
 
+	public function onBeforeUpdateForward($aArgs, &$mResult, &$mSubscriptionResult)
+	{
+		$mResult = false;
+
+		try
+		{
+			$oCpanel = $this->getCpanel();
+			if (isset($aArgs['AccountID'])
+				&& isset($aArgs['Enable'])
+				&& isset($aArgs['Email'])
+				&& !empty(trim($aArgs['Email']))
+				&& filter_var(trim($aArgs['Email']), FILTER_VALIDATE_EMAIL)
+				&& $oCpanel)
+			{
+				$sEmail = trim($aArgs['Email']);
+				//check if accountID belongs to authorized user
+				$oUser = \Aurora\System\Api::getAuthenticatedUser();
+				$oAccount = \Aurora\System\Api::GetModule('Mail')->GetAccount($aArgs['AccountID']);
+				if ($oAccount instanceof \Aurora\Modules\Mail\Classes\Account
+					&& $oUser
+					&& $oAccount->IdUser === $oUser->EntityId)
+				{
+					$sDomain = \MailSo\Base\Utils::GetDomainFromEmail($oAccount->Email);
+					//delete or create Forward depending on the Enable parameter
+					if ($aArgs['Enable'])
+					{//create forward
+						//remove forwarder if  already exists
+						$aResult = $this->getForwarder($sDomain, $oAccount->Email);
+						if (!empty($aResult) && isset($aResult['Email']))
+						{
+							$aDeletingResult = $this->deleteForwarder($oAccount->Email, $aResult['Email']);
+							if ($aDeletingResult['Status'])
+							{
+								$aCreationResult = $this->createForwarder($sDomain, $oAccount->Email, $sEmail);
+								if ($aCreationResult['Status'])
+								{
+									$mResult = true;
+								}
+								elseif (isset($aCreationResult['Error']))
+								{
+									$mSubscriptionResult = [
+										'Error' => [
+											'message'	=> $aCreationResult['Error']
+										]
+									];
+								}
+							}
+							elseif (isset($aDeletingResult['Error']))
+							{
+								$mSubscriptionResult = [
+									'Error' => [
+										'message'	=> $aDeletingResult['Error']
+									]
+								];
+							}
+						}
+						else
+						{
+							$aCreationResult = $this->createForwarder($sDomain, $oAccount->Email, $sEmail);
+							if ($aCreationResult['Status'])
+							{
+								$mResult = true;
+							}
+							elseif (isset($aCreationResult['Error']))
+							{
+								$mSubscriptionResult = [
+									'Error' => [
+										'message'	=> $aCreationResult['Error']
+									]
+								];
+							}
+						}
+						
+					}
+					else
+					{//delete forward
+						$aDeletingResult = $this->deleteForwarder($oAccount->Email, $sEmail);
+						if ($aDeletingResult['Status'])
+						{
+							$mResult = true;
+						}
+						elseif (isset($aDeletingResult['Error']))
+						{
+							$mSubscriptionResult = [
+								'Error' => [
+									'message'	=> $aDeletingResult['Error']
+								]
+							];
+						}
+					}
+				}
+			}
+		}
+		catch(\Exception $oException)
+		{}
+
+		return true; // breaking subscriptions to prevent update in parent module
+	}
+
+	public function onBeforeGetForward($aArgs, &$mResult, $mSubscriptionResult)
+	{
+		$mResult = false;
+
+		try
+		{
+			if (isset($aArgs['AccountID']))
+			{
+				//check if accountID belongs to authorized user
+				$oUser = \Aurora\System\Api::getAuthenticatedUser();
+				$oAccount = \Aurora\System\Api::GetModule('Mail')->GetAccount($aArgs['AccountID']);
+				if ($oAccount instanceof \Aurora\Modules\Mail\Classes\Account
+					&& $oUser
+					&& $oAccount->IdUser === $oUser->EntityId)
+				{
+					$sDomain = \MailSo\Base\Utils::GetDomainFromEmail($oAccount->Email);
+					$aResult = $this->getForwarder($sDomain, $oAccount->Email);
+
+					if (!empty($aResult) && isset($aResult['Email']))
+					{
+						$mResult = [
+							'Enable'	=> true,
+							'Email'	=> $aResult['Email']
+						];
+					}
+					else if (!empty($aResult) && isset($aResult['Error']))
+					{
+						$mSubscriptionResult = [
+							'Error' => [
+								'message'	=> $aResult['Error']
+							]
+						];
+					}
+				}
+			}
+		}
+		catch(\Exception $oException)
+		{}
+
+		return true; // breaking subscriptions to prevent update in parent module
+	}
+
+	public function onBeforeGetAutoresponder($aArgs, &$mResult, $mSubscriptionResult)
+	{
+		$mResult = false;
+
+		try
+		{
+			if (isset($aArgs['AccountID']))
+			{
+				//check if accountID belongs to authorized user
+				$oUser = \Aurora\System\Api::getAuthenticatedUser();
+				$oAccount = \Aurora\System\Api::GetModule('Mail')->GetAccount($aArgs['AccountID']);
+				if ($oAccount instanceof \Aurora\Modules\Mail\Classes\Account
+					&& $oUser
+					&& $oAccount->IdUser === $oUser->EntityId)
+				{
+					$aResult = $this->getAutoresponder($oAccount->Email);
+
+					if (!empty($aResult) && $aResult['Status'])
+					{
+						$mResult = [
+							'Enable'	=> $aResult['Enable'],
+							'Subject'	=> $aResult['Subject'],
+							'Message'	=> $aResult['Message']
+						];
+					}
+					else if (!empty($aResult) && isset($aResult['Error']))
+					{
+						$mSubscriptionResult = [
+							'Error' => [
+								'message'	=> $aResult['Error']
+							]
+						];
+					}
+					else
+					{
+						$mResult = [
+							'Enable' => false
+						];
+					}
+				}
+			}
+		}
+		catch(\Exception $oException)
+		{}
+
+		return true; // breaking subscriptions to prevent update in parent module
+	}
+
+	public function onBeforeUpdateAutoresponder($aArgs, &$mResult, &$mSubscriptionResult)
+	{
+		$mResult = false;
+
+		try
+		{
+			$oCpanel = $this->getCpanel();
+			if (isset($aArgs['AccountID'])
+				&& isset($aArgs['Enable'])
+				&& isset($aArgs['Subject'])
+				&& !empty(trim($aArgs['Subject']))
+				&& isset($aArgs['Message'])
+				&& !empty(trim($aArgs['Message']))
+				&& $oCpanel)
+			{
+				$sSubject = trim($aArgs['Subject']);
+				$sMessage = trim($aArgs['Message']);
+				//check if accountID belongs to authorized user
+				$oUser = \Aurora\System\Api::getAuthenticatedUser();
+				$oAccount = \Aurora\System\Api::GetModule('Mail')->GetAccount($aArgs['AccountID']);
+				if ($oAccount instanceof \Aurora\Modules\Mail\Classes\Account
+					&& $oUser
+					&& $oAccount->IdUser === $oUser->EntityId)
+				{
+					$sDomain = \MailSo\Base\Utils::GetDomainFromEmail($oAccount->Email);
+					$aResult = $this->updateAutoresponder($sDomain, $oAccount->Email, $sSubject, $sMessage, $aArgs['Enable']);
+					if ($aResult['Status'])
+					{
+						$mResult = true;
+					}
+					elseif (isset($aResult['Error']))
+					{
+						$mSubscriptionResult = [
+							'Error' => [
+								'message'	=> $aResult['Error']
+							]
+						];
+					}
+				}
+			}
+		}
+		catch(\Exception $oException)
+		{}
+
+		return true; // breaking subscriptions to prevent update in parent module
+	}
+
 	/**
 	 * Checks if allowed to change password for account.
 	 * @param \Aurora\Modules\Mail\Classes\Account $oAccount
@@ -303,5 +543,234 @@ class Module extends \Aurora\System\Module\AbstractModule
 		}
 
 		return $this->oMailModule;
+	}
+
+	protected function getForwarder($sDomain, $sEmail)
+	{
+		$aResult = [];
+
+		$oCpanel = $this->getCpanel();
+		if ($oCpanel && $sDomain && $sEmail)
+		{
+			$sResult = $oCpanel->execute_action(self::UAPI, 'Email', 'list_forwarders', $oCpanel->getUsername(),
+				[
+					'domain'	=> $sDomain,
+					'regex'	=> $sEmail
+				]
+			);
+			$oResult = \json_decode($sResult);
+
+			if ($oResult
+				&& isset($oResult->result)
+				&& isset($oResult->result->data)
+				&& is_array($oResult->result->data)
+				&& isset($oResult->result->data[0])
+			)
+			{
+				$aResult = [
+					'Email' => $oResult->result->data[0]->forward
+				];
+			}
+			else if ($oResult && isset($oResult->error))
+			{
+				$aResult = [
+					'Error' => [
+						'message'	=> $oResult->error
+					]
+				];
+			}
+			else if ($oResult && isset($oResult->result)
+				&& isset($oResult->result->errors) && !empty($oResult->result->errors)
+				&& isset($oResult->result->errors[0]))
+			{
+				$aResult = [
+					'Error' => [
+						'message'	=> $oResult->result->errors[0]
+					]
+				];
+			}
+		}
+
+		return $aResult;
+	}
+
+	protected function deleteForwarder($sAddress, $sForwarder)
+	{
+		$aResult = [
+			'Status' => false
+		];
+
+		$oCpanel = $this->getCpanel();
+		if ($oCpanel && $sAddress && $sForwarder)
+		{
+			$sResponse = $oCpanel->execute_action(self::UAPI, 'Email', 'delete_forwarder', $oCpanel->getUsername(),
+				[
+					'address'	=> $sAddress,
+					'forwarder'   => $sForwarder
+				]
+			);
+			$aResult = self::parseResponse($sResponse);
+		}
+
+		return $aResult;
+	}
+
+	protected function createForwarder($sDomain, $sEmail, $sForwardEmail)
+	{
+		$aResult = [
+			'Status' => false
+		];
+
+		$oCpanel = $this->getCpanel();
+		if ($oCpanel && $sDomain && $sEmail && $sForwardEmail)
+		{
+			$sResponse = $oCpanel->execute_action(self::UAPI, 'Email', 'add_forwarder', $oCpanel->getUsername(),
+				[
+					'domain'	=> $sDomain,
+					'email'	=> $sEmail,
+					'fwdopt'	=> 'fwd',
+					'fwdemail'   => $sForwardEmail
+				]
+			);
+			$aResult = self::parseResponse($sResponse);
+		}
+
+		return $aResult;
+	}
+
+	protected function getAutoresponder($sEmail)
+	{
+		$aResult = [
+			'Status' => false
+		];
+
+		$oCpanel = $this->getCpanel();
+		if ($oCpanel && $sEmail)
+		{
+			$sResult = $oCpanel->execute_action(self::UAPI, 'Email', 'get_auto_responder', $oCpanel->getUsername(),
+				[
+					'email' => $sEmail
+				]
+			);
+			$oResult = \json_decode($sResult);
+
+			if ($oResult
+				&& isset($oResult->result)
+				&& isset($oResult->result->data)
+				&& is_object($oResult->result->data)
+				&& isset($oResult->result->data->subject)
+			)
+			{
+				if ($oResult->result->data->stop !== NULL && $oResult->result->data->stop < time())
+				{
+					$bEnable = false;
+				}
+				else
+				{
+					$bEnable = true;
+				}
+				$aResult = [
+					'Status'	=> true,
+					'Subject'	=> $oResult->result->data->subject,
+					'Message'	=> $oResult->result->data->body,
+					'Enable'	=> $bEnable
+				];
+			}
+			else if ($oResult && isset($oResult->error))
+			{
+				$aResult = [
+					'Status'	=> false,
+					'Error'	=> [
+						'message'	=> $oResult->error
+					]
+				];
+			}
+			else if ($oResult && isset($oResult->result)
+				&& isset($oResult->result->errors) && !empty($oResult->result->errors)
+				&& isset($oResult->result->errors[0]))
+			{
+				$aResult = [
+					'Status'	=> false,
+					'Error'	=> [
+						'message'	=> $oResult->result->errors[0]
+					]
+				];
+			}
+		}
+
+		return $aResult;
+	}
+
+	protected function updateAutoresponder($sDomain, $sEmail, $sSubject, $sMessage, $bEnable)
+	{
+		$aResult = [
+			'Status' => false
+		];
+
+		$oCpanel = $this->getCpanel();
+		if ($oCpanel && $sDomain && $sEmail && $sSubject && $sMessage)
+		{
+			$iStartTime = 0;
+			$iStopTime = 0;
+			if (!$bEnable)
+			{
+				$iStopTime = time();
+				$iStartTime = $iStopTime - 1;
+			}
+			$sResponse = $oCpanel->execute_action(self::UAPI, 'Email', 'add_auto_responder', $oCpanel->getUsername(),
+				[
+					'email'	=> $sEmail,
+					'from'		=> '',
+					'subject'	=> $sSubject,
+					'body'	=> $sMessage,
+					'domain'	=> $sDomain,
+					'is_html'	=> 0,
+					'interval'	=> 0,
+					'start'		=> $iStartTime,
+					'stop'		=> $iStopTime
+				]
+			);
+			$aResult = self::parseResponse($sResponse);
+		}
+
+		return $aResult;
+	}
+
+	static function parseResponse($sResponse)
+	{
+		$aResult = [
+			'Status' => false
+		];
+
+		$oResult = \json_decode($sResponse);
+
+		if ($oResult
+			&& isset($oResult->result)
+			&& isset($oResult->result->status)
+			&& $oResult->result->status === 1
+		)
+		{
+			$aResult = [
+				'Status' => true
+			];
+		}
+		else if ($oResult && isset($oResult->error))
+		{
+			$aResult = [
+				'Status' => false,
+				'Error' => $oResult->error
+			];
+		}
+		else if ($oResult && isset($oResult->result)
+			&& isset($oResult->result->errors) && !empty($oResult->result->errors)
+			&& isset($oResult->result->errors[0]))
+		{
+			$aResult = [
+				'Status' => false,
+				'Error' => $oResult->result->errors[0]
+			];
+		}
+
+		return $aResult;
 	}
 }
