412f8065d25855e61cf1146c104d949deaab1a41
artemij-nemec <artem@chesnokov.pp.ua>
2019-07-10 15:16:20 +0300

    Added AllowWildcardDomain parameter to GetMailserverBiDomain method. Parameter indicates whether the wildcard server should be returned if no servers found

--- Module.php
+++ Module.php
@@ -686,7 +686,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 
 		if (!$bSupported)
 		{
-			$oServer = \Aurora\System\Api::GetModuleDecorator('Mail')->GetMailServerByDomain($sDomain);
+			$oServer = \Aurora\System\Api::GetModuleDecorator('Mail')->GetMailServerByDomain($sDomain, /*AllowWildcardDomain*/true);
 			if ($oServer instanceof \Aurora\Modules\Mail\Classes\Server)
 			{
 				$bSupported = in_array($oServer->IncomingServer, $this->getConfig('SupportedServers'));
