bcfecc7c7cc376856b778f58bbdc93538f7d5cc7
nadine-shi <nadinshi@gmail.com>
2021-06-18 16:38:43 +0300

    requiredModules functionality

--- vue/manager.js
+++ vue/manager.js
@@ -1,13 +1,18 @@
 import settings from '../../CpanelIntegrator/vue/settings'
 
 export default {
+  moduleName: 'CpanelIntegrator',
+
+  requiredModules: [],
+
   init (appData) {
     settings.init(appData)
   },
+
   getAdminSystemTabs () {
     return [
       {
-        name: 'cpanel',
+        tabName: 'cpanel',
         title: 'CPANELINTEGRATOR.ADMIN_SETTINGS_TAB_LABEL',
         component () {
           return import('src/../../../CpanelIntegrator/vue/components/CpanelAdminSettings')
