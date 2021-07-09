58e4040b8bb555f9cfdb6d9bb806c38422dfd85a
nadine-shi <nadinshi@gmail.com>
2021-07-09 14:57:31 +0300

    Linear progress

--- vue/components/CpanelAliasesAdminSettingsPerUser.vue
+++ vue/components/CpanelAliasesAdminSettingsPerUser.vue
@@ -42,7 +42,7 @@
       </q-card>
     </div>
     <q-inner-loading style="justify-content: flex-start;" :showing="loading || saving || deleting">
-      <q-linear-progress query class="q-mt-sm" />
+      <q-linear-progress query />
     </q-inner-loading>
     <UnsavedChangesDialog ref="unsavedChangesDialog"/>
   </q-scroll-area>
