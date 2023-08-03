94da774502ccbfe626e1a4a384dd73718b1a7e71
Vasil Sokolov <vasil@afterlogic.com>
2023-08-03 23:25:20 +0200

    Code style improvements

--- scripts/process_mail.php
+++ scripts/process_mail.php
@@ -1,11 +1,15 @@
 <?php
+/**
+ * This code is licensed under AGPLv3 license or Afterlogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
+ */
 
 if (PHP_SAPI !== 'cli') {
-    exit("Use console");
+    exit("Use the console for running this script");
 }
 
-require_once \dirname(__FILE__) . '/../../../system/autoload.php';
-
+require_once \dirname(__file__) . '/../../../system/autoload.php';
 \Aurora\System\Api::Init();
 
 $fd = fopen("php://stdin", "r");
